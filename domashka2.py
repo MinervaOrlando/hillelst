5998e43b35bd5b7ddff3e43c30f8266f8280219a
MinervaOrlando <90473829+MinervaOrlando@users.noreply.github.com>
2021-09-17 18:58:40 +0300

    Add files via upload

---
+++ domashka2.py
@@ -0,0 +1,21 @@
+#BEGIN
+print(8/2+5--3/2)
+#END
+#BEGIN
+print("Khal Drogo's favorite word is \"athjahakar\"")
+#END
+#BEGIN
+print("Winter "+"came "+"for "+"the "+"House "+"of "+"Frey ")
+#END
+#BEGIN
+word="First text"
+print(word)
+word="Second text"
+print(word)
+#END
+#BEGIN
+print("What is your name?")
+a="Hello, "
+b=input()+("!")
+print(a+b)
+#END
